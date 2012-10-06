5a7ebac996c2904ab2a2d952bdfd674026c305ac
mikeller@042.ch <mikeller@042.ch@055f53a9-ef3f-c955-aaab-bb59a23f71d4>
2012-10-06 12:08:33 +0000

    Added search for new questions on watchlist items.

--- trunk/src/ch/ike/ResultHandler.java
+++ trunk/src/ch/ike/ResultHandler.java
@@ -18,51 +18,108 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class ResultHandler {
-    private static final DocumentBuilderFactory docFactory = DocumentBuilderFactory
-	    .newInstance();
-    private static final XPathFactory xPathFactory = XPathFactory.newInstance();
+	private static final DocumentBuilderFactory docFactory = DocumentBuilderFactory
+			.newInstance();
+	private static final XPathFactory xPathFactory = XPathFactory.newInstance();
 
-    public final DocumentBuilder docBuilder;
-    public final XPathExpression itemExpr;
-    public final XPathExpression listingIdExpr;
+	public final DocumentBuilder docBuilder;
+	public final XPathExpression searchListingExpr;
+	public final XPathExpression watchlistItemExpr;
+	public final XPathExpression listingQuestionsExpr;
+	public final XPathExpression listingIdExpr;
+	public final XPathExpression titleExpr;
+	public final XPathExpression questionIdExpr;
 
-    public ResultHandler() {
-	try {
-	    docBuilder = docFactory.newDocumentBuilder();
-	} catch (ParserConfigurationException e) {
-	    throw new RuntimeException(e);
+	public ResultHandler() {
+		try {
+			docBuilder = docFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			throw new RuntimeException(e);
+		}
+
+		try {
+			searchListingExpr = xPathFactory.newXPath().compile(
+					"/SearchResults/List/Listing");
+			watchlistItemExpr = xPathFactory.newXPath().compile(
+					"/Watchlist/List/WatchlistItem");
+			listingQuestionsExpr = xPathFactory.newXPath().compile(
+					"/ListedItemDetail/Questions/List/Question");
+			listingIdExpr = xPathFactory.newXPath().compile("./ListingId");
+			titleExpr = xPathFactory.newXPath().compile("./Title");
+			questionIdExpr = xPathFactory.newXPath().compile(
+					"./ListingQuestionId");
+		} catch (XPathExpressionException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
-	try {
-	    itemExpr = xPathFactory.newXPath().compile(
-		    "/SearchResults/List/Listing");
-	    listingIdExpr = xPathFactory.newXPath().compile("./ListingId");
-	} catch (XPathExpressionException e) {
-	    throw new RuntimeException(e);
+	NodeList getSearchListings(String response) {
+		InputSource is = new InputSource(new StringReader(response));
+		try {
+			Document document = docBuilder.parse(is);
+			return (NodeList) searchListingExpr.evaluate(document,
+					XPathConstants.NODESET);
+		} catch (SAXException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (XPathExpressionException e) {
+			throw new RuntimeException(e);
+		}
 	}
-    }
 
-    NodeList parseResponse(String response) {
-        InputSource is = new InputSource(new StringReader(response));
-        try {
-            Document document = docBuilder.parse(is);
-            return (NodeList) itemExpr.evaluate(document,
-        	    XPathConstants.NODESET);
-        } catch (SAXException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (XPathExpressionException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	String getListingId(Node item) {
+		try {
+			return (String) listingIdExpr.evaluate(item, XPathConstants.STRING);
+		} catch (XPathExpressionException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    String getListingId(Node item) {
-        try {
-            return (String) listingIdExpr.evaluate(item,
-        	    XPathConstants.STRING);
-        } catch (XPathExpressionException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	NodeList getWatchlistItem(String response) {
+		InputSource is = new InputSource(new StringReader(response));
+		try {
+			Document document = docBuilder.parse(is);
+			return (NodeList) watchlistItemExpr.evaluate(document,
+					XPathConstants.NODESET);
+		} catch (SAXException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (XPathExpressionException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String getTitle(Node item) {
+		try {
+			return (String) titleExpr.evaluate(item, XPathConstants.STRING);
+		} catch (XPathExpressionException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public NodeList getListingQuestions(String response) {
+		InputSource is = new InputSource(new StringReader(response));
+		try {
+			Document document = docBuilder.parse(is);
+			return (NodeList) listingQuestionsExpr.evaluate(document,
+					XPathConstants.NODESET);
+		} catch (SAXException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (XPathExpressionException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String getQuestionId(Node item) {
+		try {
+			return (String) questionIdExpr
+					.evaluate(item, XPathConstants.STRING);
+		} catch (XPathExpressionException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
