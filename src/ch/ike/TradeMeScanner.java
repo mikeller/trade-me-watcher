4a5dc01a28fa95fcbeb8bd087da36c8b1055ed8f
Michael Keller <mikeller@users.noreply.github.com>
2012-10-06 12:08:33 +0000

    Added search for new questions on watchlist items.

--- src/ch/ike/TradeMeScanner.java
+++ src/ch/ike/TradeMeScanner.java
@@ -34,232 +34,339 @@ import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 
 public class TradeMeScanner implements Runnable {
 
-    private final Properties props;
-    private final Preferences prefs;
-    private final Preferences seenItems;
+	private final Properties props;
+	private final Preferences prefs;
+	private final Preferences seenItems;
+	private final Preferences seenQuestions;
 
-    private ResultHandler resultHandler;
-    private final TradeMeConnector connector;
+	private ResultHandler resultHandler;
+	private final TradeMeConnector connector;
 
-    private EmailProvider emailProvider;
+	private EmailProvider emailProvider;
 
-    private volatile boolean stopped;
+	private volatile boolean stopped;
 
-    public static void main(String[] args) {
-	TradeMeScanner self = new TradeMeScanner();
+	public static void main(String[] args) {
+		TradeMeScanner self = new TradeMeScanner();
 
-	if ((args.length > 0) && "deauthorise".equals(args[0])) {
-	    self.connector.deauthoriseUser();
-	} else if ((args.length > 0) && "get_access_token".equals(args[0])) {
-	    self.connector.printAccessToken();
-	} else if ((args.length > 0) && "clear_cache".equals(args[0])) {
-	    self.clearCache();
-	} else {
-	    boolean interactive = false;
-	    if ((args.length > 0) && "interactive".equals(args[0])) {
-		interactive = true;
-	    }
-	    self.runScanner(interactive);
-	}
-    }
-
-    private void clearCache() {
-	try {
-	    seenItems.removeNode();
-	    prefs.flush();
-	} catch (BackingStoreException e) {
-	    throw new RuntimeException(e);
-	}
-    }
-
-    public TradeMeScanner() {
-	props = new Properties();
-	try {
-	    FileInputStream in = new FileInputStream("TradeMeScanner.xml");
-	    props.loadFromXML(in);
-	    in.close();
-	} catch (FileNotFoundException e) {
-	    throw new RuntimeException(e);
-	} catch (InvalidPropertiesFormatException e) {
-	    throw new RuntimeException(e);
-	} catch (IOException e) {
-	    throw new RuntimeException(e);
-	}
-
-	prefs = Preferences.userNodeForPackage(this.getClass());
-
-	emailProvider = new EmailProvider(props);
-
-	connector = new TradeMeConnector(props, prefs);
-
-	seenItems = prefs.node("seen_items");
-
-	resultHandler = new ResultHandler();
-
-	stopped = false;
-    }
-
-    private void runScanner(boolean interactive) {
-	final Thread mainThread = Thread.currentThread();
-	Runtime.getRuntime().addShutdownHook(new Thread() {
-	    public void run() {
-		stopped = true;
-		mainThread.interrupt();
-
-		try {
-		    mainThread.join();
-		} catch (InterruptedException e) {
+		if ((args.length > 0) && "deauthorise".equals(args[0])) {
+			self.connector.deauthoriseUser();
+		} else if ((args.length > 0) && "get_access_token".equals(args[0])) {
+			self.connector.printAccessToken();
+		} else if ((args.length > 0) && "clear_cache".equals(args[0])) {
+			self.clearCache();
+		} else {
+			boolean interactive = false;
+			if ((args.length > 0) && "interactive".equals(args[0])) {
+				interactive = true;
+			}
+			self.runScanner(interactive);
 		}
-	    }
-	});
-
-	int interval = Integer.parseInt(props.getProperty("search.interval"));
-	System.out.println("Set search interval to " + interval + " seconds.");
-
-	Map<String, String> searches = new Hashtable<String, String>();
-	int index = 0;
-	while (props.containsKey("search." + index + ".parameters")) {
-	    String parameters = props.getProperty("search." + index
-		    + ".parameters");
-	    String title = props.getProperty("search." + index + ".title",
-		    "<unspecified>");
-	    searches.put(parameters, title);
-	    System.out.println("Added search " + title + " with parameters "
-		    + parameters);
-
-	    index = index + 1;
 	}
 
-	connector.service = new ServiceBuilder().provider(TradeMeApi.class)
-		.apiKey(props.getProperty("consumer.key"))
-		.apiSecret(props.getProperty("consumer.secret")).build();
-
-	connector.checkAuthorisation();
-
-	if (interactive) {
-	    Thread interactiveThread = new Thread(this);
-	    interactiveThread.start();
+	private void clearCache() {
+		try {
+			seenItems.removeNode();
+			seenQuestions.removeNode();
+			prefs.flush();
+		} catch (BackingStoreException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
-	while (!stopped) {
-	    Set<String> allItems;
-	    try {
-		allItems = new HashSet<String>(Arrays.asList(seenItems.keys()));
-	    } catch (BackingStoreException e) {
-		throw new RuntimeException(e);
-	    }
-	    Set<String> expiredItems = new HashSet<String>(allItems);
-	    Calendar now = GregorianCalendar.getInstance();
-	    StringBuffer message = new StringBuffer();
-	    boolean itemsFound = false;
+	public TradeMeScanner() {
+		props = new Properties();
+		try {
+			FileInputStream in = new FileInputStream("TradeMeScanner.xml");
+			props.loadFromXML(in);
+			in.close();
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (InvalidPropertiesFormatException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 
-	    for (String parameters : searches.keySet()) {
+		prefs = Preferences.userNodeForPackage(this.getClass());
+
+		emailProvider = new EmailProvider(props);
+
+		connector = new TradeMeConnector(props, prefs);
+
+		seenItems = prefs.node("seen_items");
+
+		seenQuestions = prefs.node("seen_questions");
+
+		resultHandler = new ResultHandler();
+
+		stopped = false;
+	}
+
+	private void runScanner(boolean interactive) {
+		final Thread mainThread = Thread.currentThread();
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				stopped = true;
+				mainThread.interrupt();
+
+				try {
+					mainThread.join();
+				} catch (InterruptedException e) {
+				}
+			}
+		});
+
+		int interval = Integer.parseInt(props.getProperty("search.interval"));
+		System.out.println("Set search interval to " + interval + " seconds.");
+
+		Map<String, String> searches = new Hashtable<String, String>();
+		int index = 0;
+		while (props.containsKey("search." + index + ".parameters")) {
+			String parameters = props.getProperty("search." + index
+					+ ".parameters");
+			String title = props.getProperty("search." + index + ".title",
+					"<unspecified>");
+			searches.put(parameters, title);
+			System.out.println("Added search " + title + " with parameters "
+					+ parameters);
+
+			index = index + 1;
+		}
+
+		connector.service = new ServiceBuilder().provider(TradeMeApi.class)
+				.apiKey(props.getProperty("consumer.key"))
+				.apiSecret(props.getProperty("consumer.secret")).build();
+
+		connector.checkAuthorisation();
+
+		if (interactive) {
+			Thread interactiveThread = new Thread(this);
+			interactiveThread.start();
+		}
+
+		boolean sendMessage = false;
+		StringBuffer message = new StringBuffer();
+		while (!stopped) {
+			sendMessage = searchNewListings(searches, message);
+			sendMessage = searchNewQuestions(message) || sendMessage;
+
+			if (sendMessage) {
+				emailProvider.sendEmail(message.toString());
+
+				message.setLength(0);
+				sendMessage = false;
+			}
+
+			try {
+				Thread.sleep(1000 * interval);
+			} catch (InterruptedException e) {
+			}
+		}
+
+		System.out.println("Terminated.");
+	}
+
+	private boolean searchNewQuestions(StringBuffer message) {
+		Set<String> allQuestions;
+		try {
+			allQuestions = new HashSet<String>(Arrays.asList(seenQuestions
+					.keys()));
+		} catch (BackingStoreException e) {
+			throw new RuntimeException(e);
+		}
+
+		int index;
+		Set<String> expiredQuestions = new HashSet<String>(allQuestions);
+		Calendar now = GregorianCalendar.getInstance();
+		boolean questionsFound = false;
+
 		Response response = connector
-			.sendGetRequest("https://api.trademe.co.nz/v1/Search/General.xml?"
-				+ parameters);
+				.sendGetRequest("https://api.trademe.co.nz/v1/MyTradeMe/Watchlist/All.xml");
 
-		NodeList items = resultHandler
-			.parseResponse(response.getBody());
+		NodeList watchlistItems = resultHandler.getWatchlistItem(response
+				.getBody());
 
-		message.append("New items for \"" + searches.get(parameters)
-			+ "\":\n\n");
+		int itemIndex = 0;
+		while (itemIndex < watchlistItems.getLength()) {
+			Node item = watchlistItems.item(itemIndex);
+			String itemTitle = resultHandler.getTitle(item);
 
-		index = 0;
-		int newItems = 0;
-		while (index < items.getLength()) {
-		    Node item = items.item(index);
-		    String listingId = resultHandler.getListingId(item);
+			message.append("New questions for \"" + itemTitle + "\":\n\n");
 
-		    expiredItems.remove(listingId);
-		    seenItems.put(listingId,
-			    DatatypeConverter.printDateTime(now));
-		    if (!allItems.contains(listingId)) {
-			allItems.add(listingId);
-			message.append(format(item) + "\n");
+			response = connector
+					.sendGetRequest("https://api.trademe.co.nz/v1/Listings/"
+							+ resultHandler.getListingId(item) + ".xml");
 
-			newItems = newItems + 1;
-		    }
+NodeList questions = resultHandler.getListingQuestions(response
+					.getBody());
 
-		    index = index + 1;
+			index = 0;
+			int newQuestions = 0;
+			while (index < questions.getLength()) {
+				Node question = questions.item(index);
+				String questionId = resultHandler.getQuestionId(question);
+
+				expiredQuestions.remove(questionId);
+				seenQuestions.put(questionId,
+						DatatypeConverter.printDateTime(now));
+				if (!allQuestions.contains(questionId)) {
+					allQuestions.add(questionId);
+					message.append(format(question) + "\n");
+
+					newQuestions = newQuestions + 1;
+				}
+
+				index = index + 1;
+			}
+
+			if (newQuestions > 0) {
+				message.append("\n");
+
+				questionsFound = true;
+			}
+
+			System.out.println("Found " + questions.getLength()
+					+ " questions, " + newQuestions
+					+ " new questions for watchlist item \"" + itemTitle
+					+ "\".");
+			
+			itemIndex = itemIndex + 1;
 		}
 
-		if (newItems > 0) {
-		    message.append("\n");
+		for (String questionId : expiredQuestions) {
+			Calendar lastSeen = null;
+			try {
+				lastSeen = DatatypeConverter.parseDateTime(seenQuestions.get(
+						questionId, null));
+				lastSeen.add(Calendar.DATE, 1);
+			} catch (IllegalArgumentException e) {
 
-		    itemsFound = true;
+			}
+
+			if ((lastSeen == null) || lastSeen.before(now)) {
+				seenQuestions.remove(questionId);
+			}
 		}
 
-		System.out.println("Found " + items.getLength() + " items, "
-			+ newItems + " new items for search \""
-			+ searches.get(parameters) + "\".");
-	    }
-
-	    if (itemsFound) {
-		emailProvider.sendEmail(message.toString());
-	    }
-
-	    for (String itemId : expiredItems) {
-		Calendar lastSeen = null;
 		try {
-			lastSeen = DatatypeConverter.parseDateTime(seenItems.get(itemId, null));
-			lastSeen.add(Calendar.DATE, 1);
-		} catch (IllegalArgumentException e) {
-			
+			seenQuestions.flush();
+		} catch (BackingStoreException e) {
+			throw new RuntimeException(e);
 		}
-			
-		if ((lastSeen == null) || lastSeen.before(now)) {
-		    seenItems.remove(itemId);
+		return questionsFound;
+	}
+
+	private boolean searchNewListings(Map<String, String> searches,
+			StringBuffer message) {
+		Set<String> allItems;
+		try {
+			allItems = new HashSet<String>(Arrays.asList(seenItems.keys()));
+		} catch (BackingStoreException e) {
+			throw new RuntimeException(e);
 		}
-	    }
 
-	    try {
-		seenItems.flush();
-	    } catch (BackingStoreException e) {
-		throw new RuntimeException(e);
-	    }
+		int index;
+		Set<String> expiredItems = new HashSet<String>(allItems);
+		Calendar now = GregorianCalendar.getInstance();
+		boolean itemsFound = false;
 
-	    try {
-		Thread.sleep(1000 * interval);
-	    } catch (InterruptedException e) {
-	    }
+		for (String parameters : searches.keySet()) {
+			Response response = connector
+					.sendGetRequest("https://api.trademe.co.nz/v1/Search/General.xml?"
+							+ parameters);
+
+			NodeList items = resultHandler
+					.getSearchListings(response.getBody());
+
+			message.append("New items for \"" + searches.get(parameters)
+					+ "\":\n\n");
+
+			index = 0;
+			int newItems = 0;
+			while (index < items.getLength()) {
+				Node item = items.item(index);
+				String listingId = resultHandler.getListingId(item);
+
+				expiredItems.remove(listingId);
+				seenItems.put(listingId, DatatypeConverter.printDateTime(now));
+				if (!allItems.contains(listingId)) {
+					allItems.add(listingId);
+					message.append(format(item) + "\n");
+
+					newItems = newItems + 1;
+				}
+
+				index = index + 1;
+			}
+
+			if (newItems > 0) {
+				message.append("\n");
+
+				itemsFound = true;
+			}
+
+			System.out.println("Found " + items.getLength() + " items, "
+					+ newItems + " new items for search \""
+					+ searches.get(parameters) + "\".");
+		}
+
+		for (String itemId : expiredItems) {
+			Calendar lastSeen = null;
+			try {
+				lastSeen = DatatypeConverter.parseDateTime(seenItems.get(
+						itemId, null));
+				lastSeen.add(Calendar.DATE, 1);
+			} catch (IllegalArgumentException e) {
+
+			}
+
+			if ((lastSeen == null) || lastSeen.before(now)) {
+				seenItems.remove(itemId);
+			}
+		}
+
+		try {
+			seenItems.flush();
+		} catch (BackingStoreException e) {
+			throw new RuntimeException(e);
+		}
+		return itemsFound;
 	}
 
-	System.out.println("Terminated.");
-    }
+	public void run() {
+		String input = "";
+		BufferedReader inReader = new BufferedReader(new InputStreamReader(
+				System.in));
+		while (input != null) {
+			System.out.print("Enter [x] to exit:");
+			try {
+				input = inReader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
 
-    public void run() {
-	String input = "";
-	BufferedReader inReader = new BufferedReader(new InputStreamReader(
-		System.in));
-	while (input != null) {
-	    System.out.print("Enter [x] to exit:");
-	    try {
-		input = inReader.readLine();
-	    } catch (IOException e) {
-		throw new RuntimeException(e);
-	    }
+			if ("x".equals(input)) {
+				System.exit(0);
+			}
 
-	    if ("x".equals(input)) {
-		System.exit(0);
-	    }
-
-	    System.out.println();
+			System.out.println();
+		}
 	}
-    }
 
-    private String format(Node node) {
-	try {
-	    OutputFormat format = new OutputFormat(node.getOwnerDocument());
-	    format.setLineWidth(65);
-	    format.setIndenting(true);
-	    format.setIndent(2);
-	    Writer out = new StringWriter();
-	    XMLSerializer serializer = new XMLSerializer(out, format);
-	    serializer.serialize(node);
+	private String format(Node node) {
+		try {
+			OutputFormat format = new OutputFormat(node.getOwnerDocument());
+			format.setLineWidth(65);
+			format.setIndenting(true);
+			format.setIndent(2);
+			Writer out = new StringWriter();
+			XMLSerializer serializer = new XMLSerializer(out, format);
+			serializer.serialize(node);
 
-	    return out.toString();
-	} catch (IOException e) {
-	    throw new RuntimeException(e);
+			return out.toString();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
-    }
 }
